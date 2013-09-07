e9e623cac859fe47cf4b8698126b5259b9b42e90
Anaelle Maillard <anaelle@les-maillard.com>
2013-09-07 14:27:05 +0200

    added __repr__

--- movie.py
+++ movie.py
@@ -1,7 +1,7 @@
-Class Movie(object):
-
-    def __init__(self,movie_name, movie_path, imdb_link, cover_url, summary, \
-        imdb_rating, anaelle_rating):
+class Movie(object):
+     
+    def __init__(self,movie_path, movie_name = None, imdb_link = None,\
+        cover_url = None, summary = None, imdb_rating = 0, anaelle_rating = 0):
         self.movie_name = movie_name
         self.movie_path = movie_path
         self.imdb_link = imdb_link
@@ -9,5 +9,16 @@ Class Movie(object):
         self.summary = summary
         self.imdb_rating = imdb_rating
         self.anaelle_rating = anaelle_rating
+       
 
-   
+    def __repr__(self):
+        return "<movie_name: %s, movie_path: %s, imdb_link: %s, cover_url %s, summary: %s, imdb_rating: %d, anaelle_rating: %d >" % (self.movie_name, \
+            self.movie_path, self.imdb_link, self.cover_url, self.summary, \
+            self.imdb_rating,   self.anaelle_rating)
+        
+       
+         
+        
+        
+        
+      
